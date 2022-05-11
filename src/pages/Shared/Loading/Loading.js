8422dd5695b570e9ed348c9dd9327105f9f303e9
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-11 23:33:58 +0600

    Alhamdulillah page loading for require auth setups done

--- src/pages/Shared/Loading/Loading.js
+++ src/pages/Shared/Loading/Loading.js
@@ -4,7 +4,8 @@ import { Spinner } from 'react-bootstrap';
 const Loading = () => {
     return (
         <div className='d-flex justify-content-center align-items-center my-5'>
-            <Spinner className='display-1 fw-bolder' animation="border" variant="primary" />
+            <Spinner className='display-1 fw-bolder' animation="border" variant="danger" />
+            <h2 className='text-danger'>Loading</h2>
         </div>
     );
 };
