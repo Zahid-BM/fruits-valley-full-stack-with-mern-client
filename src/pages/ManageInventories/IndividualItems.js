bfb8d89dd5e635f937c56875a8bc653998ef829c
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-13 20:13:44 +0600

    Alhamdulillah manage inventories rote shows all the items successfully

---
+++ src/pages/ManageInventories/IndividualItems.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button, Card, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import img1 from '../../images/next.png'
+
+const IndividualItems = ({ item }) => {
+    const { _id, img, name, price, description, supplierName, quantity } = item;
+    console.log(item)
+    const navigate = useNavigate();
+
+    const navigateToInventory = id => {
+        navigate(`/inventory/${id}`)
+    }
+    return (
+        <>
+            <Col className='g-4' lg={4}> {/* hover effect added */}
+                <Card className='h-100 shadow'>
+                    <Card.Img className='w-100 h-100' variant="top" src={img} />
+                    <Card.Body className='p-4 d-flex flex-column'>
+                        <Card.Title className='fw-bold fs-3 mt-3 text-danger'>{name}</Card.Title>
+                        <Card.Subtitle className='text-muted fs-4 my-3'> Price : $ {price} /kg</Card.Subtitle>
+                        <h6>Quantity : {quantity} kgs</h6>
+                        <h6 className='mb-5'>Supplier : {supplierName}</h6>
+                        <p className='fw-bold'>Description:</p>
+                        <Card.Text>
+                            {description}
+                        </Card.Text>
+
+
+                        {<div className='mt-auto' >
+                            <Button onClick={() => navigateToInventory(_id)} className='w-100 text-white fw-bold  hover1' variant="danger">Stock Update <img src={img1} alt="" /></Button>
+                        </div>}
+                    </Card.Body>
+                </Card>
+            </Col>
+        </>
+    );
+};
+
+export default IndividualItems;
--- src/pages/ManageInventories/ManageInventories.js
+++ src/pages/ManageInventories/ManageInventories.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import useInventoryItems from '../../hooks/useInventoryItems';
+import IndividualItems from './IndividualItems';
 
 const ManageInventories = () => {
-
+    const [items] = useInventoryItems();
     return (
-        <div>
-            <h1>This is Manage inventories route and this will show all the items from database.</h1>
-        </div>
+        <>
+            <Container>
+                <h1>This is Manage inventories route and this will show all the items from database.</h1>
+                <Row>
+                    {
+                        items.map(item => <IndividualItems key={item._id} item={item}></IndividualItems>)
+                    }
+                </Row>
+            </Container>
+        </>
     );
 };
 
