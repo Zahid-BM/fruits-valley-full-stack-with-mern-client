8f8bb5e9ef8cb35c6a920f8e3deb8d3d58310888
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-19 01:41:26 +0600

    Alhamdulillah code checked and revised in requested items heading

--- src/pages/RequestedItems/RequestedItems.js
+++ src/pages/RequestedItems/RequestedItems.js
@@ -23,7 +23,7 @@ const RequestedItems = () => {
             <Container className='mb-4'>
 
                 <PageTitle title={'RequestedItems'} pageColor={'blogs-bg'}></PageTitle>
-                <h1 className='text-center text-danger fw-bold display-4 mt-4'>All Requested Items by Users <br /><small className='fs-6 text-warning'>Please check the items bellow that you want to add. If not available here then visit Add Items Nav link and add your item</small></h1>
+                <h1 className='text-center text-danger fw-bold display-4 mt-4'>All Requested Items by Users <br /><small className='fs-6 text-warning'>Please check the items bellow that you want to add. If not available here then visit Request Item Nav link and add your item</small></h1>
 
                 {
                     requestedItems.length ?
