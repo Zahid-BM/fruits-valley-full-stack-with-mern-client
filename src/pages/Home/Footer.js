12dce5341c4cd17e92e6d996ac8cdb771254174a
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-24 10:09:21 +0600

    Alhamdulillah footer my-auto set for all cols to align center all the footer items

--- src/pages/Home/Footer.js
+++ src/pages/Home/Footer.js
@@ -20,14 +20,14 @@ const Footer = () => {
             <footer className='bg-danger mt-5 '>
                 <Container fluid>
                     <Row>
-                        <Col lg={4}>
+                        <Col className='my-auto' lg={4}>
                             <div className='text-center text-white py-5'>
                                 <h5>Download our Apps</h5>
                                 <img className='m-2 hover2' src={appImage1} alt="" />
                                 <img className='m-2 hover2' src={appImage2} alt="" />
                             </div>
                         </Col>
-                        <Col lg={4}>
+                        <Col className='my-auto' lg={4}>
                             <div className='text-center text-white py-5'>
                                 All right reserved &copy; Fruits Valley {year}
                                 <br />
@@ -37,7 +37,7 @@ const Footer = () => {
                                 <h4><img src={appImage6} alt="" /><span className='text-warning'>Fruits Valley</span> a reliable Warehouse</h4>
                             </div>
                         </Col>
-                        <Col lg={4}>
+                        <Col className='my-auto' lg={4}>
                             <div className='text-center text-white py-5'>
                                 <h5>Find us on</h5>
                                 <img className='m-2 hover2' src={appImage3} alt="" />
@@ -48,13 +48,6 @@ const Footer = () => {
                     </Row>
                 </Container>
             </footer>
-
-
-
-
-
-
-
         </>
     );
 };
